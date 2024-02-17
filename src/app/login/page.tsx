0a4cb341c6c1c2684c7f4f87e094edafde0b70d4
Brandon <bcruz12127@gmail.com>
2024-02-17 16:58:10 -0500

    built basic login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,16 @@
-import { Box } from '@mui/material'
+import { Box, TextField, Button, FormLabel } from '@mui/material'
 import React from 'react'
+import { useStyles } from './styles'
 
 const Login = () => {
+  const { root, textField, button } = useStyles
   return (
-    <>
-    <Login />
-    </>
+    <Box component="form" style={root} >
+    <FormLabel sx={{fontSize: '24px', textAlign: 'center'}}>Login</FormLabel>
+    <TextField label="Username"  style={textField}/>
+    <TextField label="Password" type="password" style={textField}/>
+    <Button style={button}>Login</Button>
+    </Box>
   )
 }
 
---
+++ src/app/login/styles.ts
@@ -0,0 +1,13 @@
+export const useStyles = {
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    } as React.CSSProperties,
+    textField: {
+        width: '10%',
+    },
+    button: {
+        width: '10%',
+    },
+    };
