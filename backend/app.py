e2fa2d8ad817149b7906d00ef8edb61668f7fa5c
Brandon <bcruz12127@gmail.com>
2024-02-23 01:30:10 -0500

    Co-authored-by: Kenneth Riveron <kenneth.riveron@gmail.com>

--- backend/app.py
+++ backend/app.py
@@ -8,19 +8,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'betMate.db')
 db = SQLAlchemy(app)
 
-@app.cli.command("create_db")
+@app.cli.command("db_create")
 def create_db():
     db.create_all()
     print("Database created!")
 
-@app.cli.command("drop_db")
+@app.cli.command("db_drop")
 def drop_db():
     db.drop_all()
     print("Database dropped!")
 
-@app.cli.command("seed_db")
+@app.cli.command("db_seed")
 def seed_db():
-    from models import User
+    from backend.models.models import User
     user1 = User(username="user1", email="b@mail.com", password="123", team="team1")
     db.session.add(user1)
     db.session.commit()
