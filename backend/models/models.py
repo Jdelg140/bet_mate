ad76b6baca5055cadef397abc1ad2e5f9d28ceb6
Jacob Delgado <128998284+Jdelg140@users.noreply.github.com>
2024-03-01 16:38:52 -0500

    fixed flask and sql seeding

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,19 +1,45 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Integer, Column
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, CHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'betMate.db')
-db = SQLAlchemy(app)
+# app = Flask(__name__)
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column('id', Integer, primary_key=True)
+    username = Column('username', String, nullable=False)
+    email = Column('email', String, nullable=False)
+    password = Column('password', String, nullable=False)
+    team = Column('team', String, nullable=False)
+
+    def __init__(self, username, email, password, team):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.team = team
+
+    def __repr__(self):
+        return f"({self.id}, {self.username}, {self.email}, {self.password}, {self.team})"
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    team = Column(String, nullable=False)
+engine = create_engine('sqlite:///betMate.db', echo=True)
+Base.metadata.create_all(bind=engine)
 
+Session = sessionmaker(bind=engine)
+session = Session()
+
+user1 = User(username="brandon", email="bran@mail.com", password="123", team="team1")
+user2 = User(username="Jacob", email="jake@mail.com", password="456", team="team2")
+user3 = User(username="Kenny", email="ken@mail.com", password="789", team="team3")
+session.add(user1)
+session.add(user2)
+session.add(user3)
+session.commit()
+
+results = session.query(User).all()
+print(results)
