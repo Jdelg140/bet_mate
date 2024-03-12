c851f3ca33d1975cbd6bc413fc2c8810caa27c74
Jacob Delgado <128998284+Jdelg140@users.noreply.github.com>
2024-03-11 20:41:56 -0400

    added components to chat page

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -6,8 +6,16 @@ import { useStyles } from './styles';
 
 import { ChatListComp } from '@/components/common/ChatPage/chatListComp';
 
-
 export const ChatComp = () => {
-    const { root } = useStyles;
-  return <Box sx={root}><ChatListComp/></Box>;
+  const { root, mainContainer, listContainer } = useStyles;
+  return (
+    <Box sx={root}>
+      <Box sx={mainContainer}>
+        <Box></Box>
+        <Box sx={listContainer}>
+          <ChatListComp />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
